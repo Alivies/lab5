a29c0f8b0b773ea21758f1e4fb0e294851261654
Alivies <dpozdd@icloud.com>
2022-06-27 13:18:41 +0700

    Добавьте файлы проекта.

---
+++ lab5/Program.cs
@@ -0,0 +1,14 @@
+﻿/*******************************************
+ * Автор: Поздняков Д.А.				   *
+ * Дата: 30.04.2022                        *
+ * Название: Строки и коллекции            *
+********************************************/
+
+namespace lab5 {
+	class Program {
+		static void Main(string[] args) {
+			var Corrector = new Corrector();
+			Corrector.Program();
+		}
+	}
+}
